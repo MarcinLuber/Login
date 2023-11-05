80b0186348ee506e49773040226d3241757ad666
MarcinLuber <kaharotto.ml@gmail.com>
2023-11-05 20:31:39 +0100

    changes design + error ejs

--- src/index.js
+++ src/index.js
@@ -1,83 +1,74 @@
+// server.js
+
 const express = require('express');
 const bcrypt = require('bcrypt');
-const collection = require('./config')
-
+const collection = require('./config');
 
 const app = express();
-//convert data to json format
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
-
-//use EJS as the view engine
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('login');
+    res.render('login', { message: null });
 });
 
 app.get('/signup', (req, res) => {
-    res.render('signup')
-})
+    res.render('signup', { message: null });
+});
 
 app.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-//register new user
-app.post('/signup', async (req, res) => {
 
+
+
+
+app.post('/signup', async (req, res) => {
     const data = {
         name: req.body.username,
         password: req.body.password
     }
-    //check users name double
-    const userCheck = await collection.findOne({ name: data.name })
+
+    const userCheck = await collection.findOne({ name: data.name });
 
     if (userCheck) {
-        // res.render('home');
-        app.get('/');
-        // next();
-        res.send(`<a href="/signup"a>Powróc na strone główną</>
-        <p>User already exist. Plase choose a different username</p>`)
+        res.render('signup', { message: 'Użytkownik już istnieje. Proszę wybrać inną nazwę użytkownika.' });
     } else {
-        //hash pasword with bcrypt
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(data.password, saltRounds);
-
         data.password = hashedPassword;
 
         const userdata = await collection.insertMany(data);
         console.log(userdata);
+        res.render('login', { message: 'Pomyślnie zarejestrowano. Możesz teraz się zalogować.' });
     }
 });
-
-//login user
 
 app.post('/login', async (req, res) => {
     try {
         const check = await collection.findOne({ name: req.body.username });
+
         if (!check) {
-            res.send('user name cannot found')
-        }
-        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
-        if (isPasswordMatch) {
-            res.render('home')
+            res.render('login', { message: 'Nazwa użytkownika nie znaleziona' });
         } else {
-            req.send('wrong password')
+            const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+
+            if (isPasswordMatch) {
+                res.render('home');
+            } else {
+                res.render('login', { message: 'Nieprawidłowe hasło' });
+            }
         }
     } catch {
-        res.send('wrong details')
+        res.render('login', { message: 'Wystąpił błąd' });
     }
-})
-
-
-
+});
 
 
 const port = 5000;
 app.listen(port, () => {
-    console.log(`server runing on: ${port}`);
-})
+    console.log(`Server running on: http://localhost:${port}`);
+});
